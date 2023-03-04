6a7e60cfd0f3f1d5e1f8f907343c2ff16b4fc8b6
NetesaRoman <netesroman8@gmail.com>
2023-03-04 15:25:13 +0200

    User model and migration

--- src/main/java/com/example/petproj/PetprojApplication.java
+++ src/main/java/com/example/petproj/PetprojApplication.java
@@ -1,13 +1,32 @@
 package com.example.petproj;
 
+import com.example.petproj.dto.UserDto;
+import com.example.petproj.model.UserRole;
+import com.example.petproj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class PetprojApplication {
 
+	@Autowired
+	private UserService userService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PetprojApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void start(){
+
+
+
+
+
+
 	}
 
 }
