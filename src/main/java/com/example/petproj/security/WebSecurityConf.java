64cfb519c13f7714b1cced78bc16203e5fb18b2c
NetesaRoman <netesroman8@gmail.com>
2023-03-06 14:14:33 +0200

    UserRegistration almost done + css not showing fix

--- src/main/java/com/example/petproj/security/WebSecurityConf.java
+++ src/main/java/com/example/petproj/security/WebSecurityConf.java
@@ -1,9 +1,16 @@
 package com.example.petproj.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /*
  *
@@ -17,7 +24,42 @@ public class WebSecurityConf {
     private UserDetailsServiceImpl userDetailsService;
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers( "/login", "/welcome", "/registration", "/styles/**", "/img/**").permitAll()
+
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/welcome")
+                        .permitAll()
+                )
+                .exceptionHandling().accessDeniedPage("/403").and()
+                .logout(LogoutConfigurer::permitAll);
+
+
+        return http.build();
     }
 }
